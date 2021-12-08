7685ae6252f98d333758b7587299cd0af1214afa
Subuthai <39835845+Subuthai@users.noreply.github.com>
2021-12-09 00:10:56 +0300

    Preparing for mute command.

--- src/bot.js
+++ src/bot.js
@@ -1,13 +1,14 @@
 import { Client, Collection } from "discord.js";
 import events from "./utils/events.js";
 import commands from "./utils/commands.js";
+import database from "./utils/database.js";
 import { config } from "dotenv";
 
 config({path: ".env"});
 
-
 export const client = new Client({intents: 32509});
 client.commands = new Collection();
+await database.connect(process.env.MONGODB_URL);
 await events.loadEvents();
 await commands.loadCommands();
 await client.login(process.env.CLIENT_TOKEN);
