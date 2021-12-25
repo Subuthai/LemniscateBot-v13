78b2b831ee703fcbe9bb31b5b6b657043df385fc
Subuthai <mryargic@gmail.com>
2021-12-25 09:25:32 +0300

    Button handling.

--- src/bot.js
+++ src/bot.js
@@ -1,14 +1,17 @@
-import { Client, Collection } from "discord.js";
-import events from "./utils/events.js";
-import commands from "./utils/commands.js";
-import database from "./utils/database.js";
-import { config } from "dotenv";
+import { Client, Collection } from 'discord.js'
+import events from './utils/events.js'
+import commands from './utils/commands.js'
+import buttons from './utils/buttons.js'
+import database from './utils/database.js'
+import { config } from 'dotenv'
 
-config({ path: ".env" });
+config({ path: '.env' })
 
-export const client = new Client({ intents: 32509 });
-client.commands = new Collection();
-await database.connect(process.env.MONGODB_URL);
-await events.loadEvents();
-await commands.loadCommands();
-await client.login(process.env.CLIENT_TOKEN);
+export const client = new Client({ intents: 32509 })
+client.commands = new Collection()
+client.buttons = new Collection()
+await database.connect(process.env.MONGODB_URL)
+await events.loadEvents()
+await commands.loadCommands()
+await buttons.loadButtons()
+await client.login(process.env.CLIENT_TOKEN)
