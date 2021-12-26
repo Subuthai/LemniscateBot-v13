192fa34d68523a174010ebcfa4ca21f80fca1e6d
Subuthai <mryargic@gmail.com>
2021-12-26 08:21:51 +0300

    Added more random pics commands.

---
+++ src/commands/User/Random Pictures/bird.js
@@ -0,0 +1,23 @@
+import { SlashCommandBuilder } from '@discordjs/builders'
+import { CommandInteraction, MessageEmbed } from 'discord.js'
+import request from 'request'
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('bird')
+        .setDescription('Sends a random bird image.'),
+    /** @param {CommandInteraction} interaction */
+    execute: async function(interaction) {
+        request('https://some-random-api.ml/img/bird', async function (error, body) {
+            let result = JSON.parse(body.body);
+            const embed = new MessageEmbed({
+                title: '🐦',
+                color: 'RANDOM',
+                image: {
+                    url: result.link
+                }
+            })
+            await interaction.reply({ embeds: [embed] })
+        })
+    }
+}
